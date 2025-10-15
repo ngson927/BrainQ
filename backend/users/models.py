ae97a38cbc9a137bdf743ed50dee08f34a287c96
haminaaziz22-sketch <haminaaziz22@gmail.com>
2025-10-14 23:00:59 -0400

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -43,21 +43,25 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
     role = models.CharField(max_length=10, default="user")
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_suspended = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
 
-    objects = CustomUserManager()  
+    objects = CustomUserManager()
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email"]
 
     class Meta:
-        db_table = "users" 
+        db_table = "users"
 
     def __str__(self):
         return self.username
+
 
 
 # =========================
